177493341b553b1dae2bed4193a253e9d8d2c3b0
Cisco Vlahakis <ciscovlahakis@gmail.com>
2024-02-07 16:00:16 +0000

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+  devise_for :users
+  authenticated :user do
+    root 'deliveries#index', as: :authenticated_root
+  end
+
+  root 'deliveries#index'
+
+  # CREATE
+  post("/insert_delivery", { :controller => "deliveries", :action => "create" })
+          
+  # READ
+  get("/deliveries", { :controller => "deliveries", :action => "index" })
+  
+  get("/deliveries/:path_id", { :controller => "deliveries", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_delivery/:path_id", { :controller => "deliveries", :action => "update" })
+  
+  # DELETE
+  get("/delete_delivery/:path_id", { :controller => "deliveries", :action => "destroy" })
+
+  #------------------------------
+
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
